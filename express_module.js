c60c59d92ebe3320d3582352d56346d4d0475e27
Jonnadula Chaitanya <sparkscj234@gmail.com>
2022-02-02 12:50:49 +0530

    express server

--- express_module.js
+++ express_module.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const http = require('http');
+const path = require('path');
+
 const morgan = require('morgan');
+const fs = require('fs');
 const hostname = 'localhost';
 const port = 8080;
 
@@ -9,9 +12,18 @@ app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 
 app.use((req,res,next)=>{
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.write('<html><body><p>This is express server</p></body></html>');
-    res.end();
+    var fileUrl;
+    if (req.url == '/') fileUrl = '/index.html'
+    else fileUrl = req.url
+
+    var filepath = path.resolve('./public' + fileUrl);
+
+    // res.writeHead(200,{'Content-Type':'text/html'});
+    // res.write('<html><body><p>This is express server</p></body></html>');
+    // res.end();
+    fs.createReadStream(filepath).pipe(res)
+    // res.send(filepath)
+    next();
 });
 
 
